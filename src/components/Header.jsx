c28e1b18610a6eda6dcf6a7f3ab29f17d750af47
mannuccimattia <mannuccimattia@gmail.com>
2025-06-03 15:27:19 +0200

    [UPD] Done Header style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,13 @@ import { Link } from "react-router-dom"
 
 const Header = () => {
   return (
-    <nav className="navbar bg-secondary mb-4">
+    <nav className="navbar bg-success">
       <div className="container-fluid">
         <Link className="navbar-brand text-white" to="/">
-          BoolFlix
+          <div className="d-flex align-items-center">
+            <img src="../src/assets/imgs/logo.png" alt="logo" id="header-logo" />
+            <div>BoolFlix</div>
+          </div>
         </Link>
       </div>
     </nav>
