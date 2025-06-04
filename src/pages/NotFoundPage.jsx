a395252497925769300cefd702f6e5b730aa39f5
mannuccimattia <mannuccimattia@gmail.com>
2025-06-04 15:57:51 +0200

    [FIX] Changed NotFoundPage return button color

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -6,7 +6,7 @@ const NotFoundPage = () => {
       <h1 className="fw-semibold">404</h1>
       <h3 >Page not Found</h3>
       <p className="mt-3 text-secondary fw-light">The page you requested does not exist</p>
-      <Link to="/" className="btn btn-primary mt-5">Return to homepage</Link>
+      <Link to="/" className="btn btn-success mt-5">Return to homepage</Link>
     </div>
   )
 }
