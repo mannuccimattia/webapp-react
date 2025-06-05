e3df4fb553f686a64b9b6e60f303fb831e0c58e5
mannuccimattia <mannuccimattia@gmail.com>
2025-06-05 15:42:46 +0200

    [UPD]Added validation to ReviewForm

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -3,26 +3,69 @@ import { useState } from "react";
 
 const ReviewForm = ({ book_id, reloadReviews }) => {
 
+  // variables
   const initialData = { name: "", vote: "", text: "" };
 
+  // state variables
   const [formData, setFormData] = useState(initialData);
+  const [isFormValid, setIsFormValid] = useState(true);
+  const [errorMessage, setErrorMessage] = useState([]);
 
+  // function for storing form data
   const setFieldValue = (e) => {
     const { value, name } = e.target;
 
     setFormData({ ...formData, [name]: value });
   };
 
+  // function for form data validation, returns true if all fields are correct
+  const validateForm = () => {
+    const errors = [];
+
+    // whenever a field is empty, push an error
+    if (!formData.name) {
+      errors.push({ field: "name", message: "Name can't be empty" });
+    }
+    if (!formData.text) {
+      errors.push({ field: "text", message: "Text can't be empty" });
+    }
+    if (!formData.vote) {
+      errors.push({ field: "vote", message: "Vote can't be empty" });
+    }
+    if (isNaN(formData.vote) || formData.vote < 0 || formData.vote > 5) {
+      errors.push({ field: "vote", message: "Vote must be a number from 0 to 5" });
+    }
+
+    // if errors array is not empty, returns false
+    if (errors.length > 0) {
+      setErrorMessage(errors);
+      return false;
+    }
+
+    // if no errors, returns true
+    return true;
+  };
+
+  // function for submitting form data to db
   const handleSubmit = (e) => {
     e.preventDefault();
 
+    // if validation fails, update state and exit
+    if (!validateForm()) {
+      setIsFormValid(false);
+      return;
+    }
+
+    // if valid, do ajax call for storing the review
     axios.post(`http://127.0.0.1:3000/api/movies/${book_id}/review`, formData, {
       headers: { "Content-Type": "application/json" }
     }).then(() => {
+      setIsFormValid(true);
       setFormData(initialData);
       reloadReviews();
     })
   }
+
 
   return (
     <div className='col-12'>
@@ -33,6 +76,9 @@ const ReviewForm = ({ book_id, reloadReviews }) => {
         </div>
 
         <div className="card-body">
+          {!isFormValid && <div className="alert alert-danger border-danger bg-black mb-3">
+            <strong>Form data not valid</strong>
+          </div>}
           <form onSubmit={handleSubmit}>
             <div className="row gy-3">
               <div className="col-12">
@@ -45,24 +91,34 @@ const ReviewForm = ({ book_id, reloadReviews }) => {
                   id='name'
                   value={formData.name}
                   onChange={setFieldValue}
-                  required
                 />
+                {errorMessage.map(err => {
+                  if (err.field === "name") return (
+                    <span key={`${err.field}-key`} className="text-danger opaque-mid mt-2">
+                      {err.message}
+                    </span>
+                  )
+                })}
               </div>
 
               <div className="col-12">
                 <label htmlFor="vote" className='control-label opaque-mid mb-1'>Vote</label>
                 <input
                   type="number"
-                  min="0"
-                  max="5"
                   className='form-control'
                   placeholder='Vote'
                   name='vote'
                   id='vote'
                   value={formData.vote}
                   onChange={setFieldValue}
-                  required
                 />
+                {errorMessage.map(err => {
+                  if (err.field === "vote") return (
+                    <span key={`${err.field}-key`} className="text-danger opaque-mid mt-2">
+                      {err.message}
+                    </span>
+                  )
+                })}
               </div>
 
               <div className="col-12">
@@ -75,8 +131,14 @@ const ReviewForm = ({ book_id, reloadReviews }) => {
                   id='text'
                   value={formData.text}
                   onChange={setFieldValue}
-                  required
                 ></textarea>
+                {errorMessage.map(err => {
+                  if (err.field === "text") return (
+                    <span key={`${err.field}-key`} className="text-danger opaque-mid mt-2">
+                      {err.message}
+                    </span>
+                  )
+                })}
               </div>
 
               <div className="col-12 mt-4">
